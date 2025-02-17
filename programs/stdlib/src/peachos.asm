fd0afccc498659e0a905fe9ea0d11c93cf62f881
ShivamShah1 <93140921+ShivamShah1@users.noreply.github.com>
2025-02-17 00:49:47 -0800

    User programs and shared libraries 3.0
    TIll here, I completed custom bootloader, kernel, custom compiler, linker script, memory allocation and freeing, interrupts, input/output, paging and mapping, disk reading, file management, disk streaming, FAT16 implementation by reading a file during boot, filesystem and descriptor, kernel panic, GDT(Global Descriptive Table), TSS(Task Switch Segment), user task and user process creation, interrupt handling for kernel and user space switching, printing string from user space (first user program run), virtual keyboard buffer for separate process, keyboard driver for printing from character from user space and program on screen and terminal, creating an ELF and accessing it to run the system, user program and standard libraries.

--- programs/stdlib/src/peachos.asm
+++ programs/stdlib/src/peachos.asm
@@ -3,9 +3,10 @@
 section .asm
 
 global print:function
-global getkey:function
+global peachos_getkey:function
 global peachos_malloc:function
 global peachos_free:function
+global peachos_putchar:function
 
 ; void print(const char* message)
 print:
@@ -20,13 +21,24 @@ print:
     ret
 
 ; int getkey()
-getkey:
+peachos_getkey:
     push ebp
     mov ebp, esp
     mov eax, 2 ; command getkey
     int 0x80
     pop ebp
     ret
+
+; void peachos_putchar(char c) 
+peachos_putchar:
+    push ebp
+    mov ebp, esp
+    mov eax, 3 ; command putchar
+    push dword[ebp+8] ; variable c
+    int 0x80
+    add esp, 4
+    pop ebp
+    ret 
 
 ; void* peachos_malloc(size_t size)
 peachos_malloc:
---
+++ programs/stdlib/src/stdio.h
@@ -0,0 +1,10 @@
+/*
+    stdlib file for OS
+*/
+#ifndef PEACHOS_STDIO
+#define PEACHOS_STDIO
+
+int putchar(int c);
+int printf(const char* fmt, ...);
+
+#endif
